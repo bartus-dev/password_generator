0919938479ad182c215dc60db010bbae5e7b3bd3
Bartek <sochan.bartek@gmail.com>
2026-07-19 22:19:32 +0200

    Refactor generator into a function and add gitignore

--- generator.py
+++ generator.py
@@ -1,20 +1,23 @@
 import random
 
-data = "ABCDEFGHIJKLMNOPRTSUWYZabcdefghijklmnoprstuwyz1234567890!@#$%^&*()"
+def generate():
+    data = "ABCDEFGHIJKLMNOPRTSUWYZabcdefghijklmnoprstuwyz1234567890!@#$%^&*()"
+    password = ""
+    i = 0
 
-password = ""
+    try:
+        password_length = int(input("Choose length of your password: "))
 
-i = 0
+        if password_length <= 0:
+            return "\nPassword length must be greater than 0!\n"
+        else:
+            while i < password_length:
+                password += random.choice(data)
+                i += 1
 
-try:
-    password_length = int(input("Choose length of your password: "))
-    if password_length <= 0:
-        print("\nPassword length must be greater than 0!\n")
-    else:
-        while i < password_length:
-            password += random.choice(data)
-            i += 1
+            return f"Your password: {password}"
+        
+    except ValueError:
+        return "\nPassword length cannot be represented by anything other than an integer\n"
 
-        print(f"Your password: {password}")
-except ValueError:
-    print("\nPassword length cannot be represented by anything other than an integer\n")
+print(generate())
