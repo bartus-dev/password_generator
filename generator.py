4d45c599e3fedc00473d2393b2520015b8b311e6
Bartek <sochan.bartek@gmail.com>
2026-09-16 21:46:17 +0200

    Add configurable character categories and yes/no prompt helper

--- generator.py
+++ generator.py
@@ -1,23 +1,59 @@
 import secrets
 
+def ask_yes_no(question):
+    while True:
+        choice = input(question)
+        if choice == "y":
+            return True
+        elif choice == "n":
+            return False
+        else:
+            print("Wrong choice, try again!")
+            continue
+
+
 def generate():
-    data = "ABCDEFGHIJKLMNOPRTSUWYZabcdefghijklmnoprstuwyz1234567890!@#$%^&*()"
+    lower_case = "abcdefghijklmnoprstuwyz"
+    upper_case = "ABCDEFGHIJKLMNOPRSTUWYZ"
+    digits = "1234567890"
+    symbols = "!@#$%^&*()"
+    data = ""
     password = ""
     i = 0
 
+
     try:
-        password_length = int(input("Choose length of your password: "))
+        options = [
+            ("Lowercase letters? (y/n): ", lower_case, "lowercase letters"),
+            ("Uppercase letters? (y/n): ", upper_case, "uppercase letters"),
+            ("Digits? (y/n): ", digits, "digits"),
+            ("Symbols? (y/n): ", symbols, "symbols")
+        ]
 
-        if password_length <= 0:
-            return "\nPassword length must be greater than 0!\n"
-        else:
-            while i < password_length:
-                password += secrets.choice(data)
-                i += 1
+        while True:
+            for question, characters, category_name in options:
+                if ask_yes_no(question):
+                    data += characters
+                    print("Added: ", category_name)
+                else:
+                    print("Skipped: ", category_name)
 
-            return f"Your password: {password}"
+            if not data:
+                print("You need to select at least one character type.\n")
+                continue
+            else:
+                password_length = int(input("Choose length of your password: "))
+                
+                if password_length <= 0:
+                    return "\nPassword length must be greater than 0!\n"    
+                else:
+                    while i < password_length:
+                        password += secrets.choice(data)
+                        i += 1
+
+                    return f"\nYour password: {password}"
         
     except ValueError:
-        return "\nPassword length cannot be represented by anything other than an integer\n"
+        return "Please enter a valid whole number.\n"
 
 print(generate())
